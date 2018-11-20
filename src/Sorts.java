2895c05322bdd5bb00a904dcaaf73d892f335e5a
Eric Xu <exu4630@bths.edu>
2018-11-20 14:16:30 -0500

    Add .gitignore file.

--- src/Sorts.java
+++ src/Sorts.java
@@ -25,9 +25,39 @@ public class Sorts
     public static void printArray(int[] arr)
     {
         for(int v : arr)
-            System.out.print(v + ", ");
+            System.out.print(v + " ");
 
-        System.out.println();
+
+    }
+
+
+
+    public static void ascendingSort(String[] arr)
+    {
+        for(int i = 0; i<arr.length - 1; i++)
+        {
+            int lowPosition = i;
+            for (int j = i+1; j < arr.length; j++)
+            {
+                if(arr[j].compareTo(arr[lowPosition]) < 0)
+                {
+                    lowPosition = j;
+                }
+            }
+            swap(i,lowPosition,arr);
+        }
+    }
+    private static void swap(int indexOne, int indexTwo, String[] arr)
+    {
+        String temp = arr[indexOne];
+        arr[indexOne] = arr[indexTwo];
+        arr[indexTwo] = temp;
+    }
+
+    public static void printArray(String[] arr)
+    {
+        for(String s : arr)
+            System.out.print(s + " ");
 
     }
 
--- src/Test.java
+++ src/Test.java
@@ -4,9 +4,20 @@ public class Test
     {
         int[] arr1 = {159,852,6598,751,565,565,559,213};
         Sorts.printArray(arr1);
+        System.out.println();
 
         Sorts.ascendingSort(arr1);
         Sorts.printArray(arr1);
+        System.out.println();
+        System.out.println();
+
+        String[] stringArr1 = {"Bob", "John", "Eric", "Steve", "Justin"};
+        Sorts.printArray(stringArr1);
+        System.out.println();
+        Sorts.ascendingSort(stringArr1);
+        Sorts.printArray(stringArr1);
+
+
 
     }
 
